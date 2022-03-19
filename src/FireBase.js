99a32825d1472c39c746fb30468add9721f94f2e
Matt <i.w.b.s.bilm@gmail.com>
2022-03-19 14:38:26 -0700

    started...

---
+++ src/FireBase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA4sZ_gaJEZgHZ7HlyX_7LKNh_dJRmksPM",
+    authDomain: "whatsapp-7ad09.firebaseapp.com",
+    projectId: "whatsapp-7ad09",
+    storageBucket: "whatsapp-7ad09.appspot.com",
+    messagingSenderId: "1026731372556",
+    appId: "1:1026731372556:web:429a86c4f7b560806dfe29",
+    measurementId: "G-C9JL4Z4CQ7"
+  };
